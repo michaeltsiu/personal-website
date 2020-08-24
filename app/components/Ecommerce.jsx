1a8657f7c1ce95df312c0cb8c2c127fc297c0374
Michael Siu <michaeltsiu13@gmail.com>
2020-08-24 15:01:39 -0700

    Added a scroll down feature below hero content

--- app/components/Ecommerce.jsx
+++ app/components/Ecommerce.jsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { createRef } from 'react';
+
+import { MdKeyboardArrowDown } from "react-icons/md";
+
+let ref = React.createRef();
 
 class Ecommerce extends React.Component {
   constructor(props) {
@@ -6,14 +10,24 @@ class Ecommerce extends React.Component {
     this.state = {
 
     }
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick() {
+    ref.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+
   render() {
     return (
-      <div>
-        <div className="portfolio-main">
-          <video src="https://michael-siu-portfolio.s3-us-west-1.amazonaws.com/FEC.mp4" alt="GIF NOT FOUND" width="75%" height="auto" max-width="30vw" loop="loop" autoPlay="autoplay" />
-        </div>
-        <div>
+      <div className="portfolio-main">
+        <div className="portfolio-hero">
+          <div className="portfolio-video">
+            <video src="https://michael-siu-portfolio.s3-us-west-1.amazonaws.com/FEC.mp4" alt="VIDEO NOT FOUND" width="75%" height="auto" max-width="30vw" loop="loop" autoPlay="autoplay" />
+          </div>
+
           <div className="portfolio-tech">
             <div>
               <span>Built with:</span>
@@ -22,16 +36,55 @@ class Ecommerce extends React.Component {
                 <li>Express</li>
                 <li>MySQL</li>
                 <li>Node.js</li>
-                <li>hosted on AWS EC2 & RDS</li>
+                <li>Hosted on AWS EC2 & RDS</li>
               </ul>
             </div>
           </div>
+
+          <div className="portfolio-arrow">
+            <div onClick={this.handleClick}>
+             <MdKeyboardArrowDown />
+            </div>
+          </div>
+
         </div>
 
-        <a href="https://github.com/proj-plus-ultra/fenty-recommended" target="_blank">codebase</a>
+        <div className="ecommerce-body" ref={ref}>
+          <div className="ecommerce-header">From the beginning</div>
+          <div></div>
+
+          <div>You can find the codebase of the E-commerce clone project <a href="https://github.com/proj-plus-ultra/fenty-recommended" target="_blank">
+          here
+        </a></div>
+        </div>
       </div>
     )
   }
+  // render() {
+  //   return (
+  //     <div>
+  //       <div className="portfolio-main">
+  //         <video src="https://michael-siu-portfolio.s3-us-west-1.amazonaws.com/FEC.mp4" alt="GIF NOT FOUND" width="75%" height="auto" max-width="30vw" loop="loop" autoPlay="autoplay" />
+  //       </div>
+  //       <div>
+  //         <div className="portfolio-tech">
+  //           <div>
+  //             <span>Built with:</span>
+  //             <ul>
+  //               <li>React</li>
+  //               <li>Express</li>
+  //               <li>MySQL</li>
+  //               <li>Node.js</li>
+  //               <li>hosted on AWS EC2 & RDS</li>
+  //             </ul>
+  //           </div>
+  //         </div>
+  //       </div>
+
+  //       <a href="https://github.com/proj-plus-ultra/fenty-recommended" target="_blank">codebase</a>
+  //     </div>
+  //   )
+  // }
 }
 
 export default Ecommerce;
