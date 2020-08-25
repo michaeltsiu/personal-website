0c0553318e11279c5e15274e86e1e1b4b931758a
Michael Siu <michaeltsiu13@gmail.com>
2020-08-25 16:31:10 -0700

    Fixed arrow placement in Ecommerce

--- app/components/Ecommerce.jsx
+++ app/components/Ecommerce.jsx
@@ -46,13 +46,13 @@ class Ecommerce extends React.Component {
              <MdKeyboardArrowDown />
             </div>
           </div>
-
         </div>
 
         <div className="ecommerce-body" ref={ref}>
           <div className="ecommerce-header">From the beginning</div>
-          <div></div>
-
+          <div className="ecommerce-body">
+            <p></p>
+          </div>
           <div>You can find the codebase of the E-commerce clone project <a href="https://github.com/proj-plus-ultra/fenty-recommended" target="_blank">
           here
         </a></div>
@@ -60,31 +60,6 @@ class Ecommerce extends React.Component {
       </div>
     )
   }
-  // render() {
-  //   return (
-  //     <div>
-  //       <div className="portfolio-main">
-  //         <video src="https://michael-siu-portfolio.s3-us-west-1.amazonaws.com/FEC.mp4" alt="GIF NOT FOUND" width="75%" height="auto" max-width="30vw" loop="loop" autoPlay="autoplay" />
-  //       </div>
-  //       <div>
-  //         <div className="portfolio-tech">
-  //           <div>
-  //             <span>Built with:</span>
-  //             <ul>
-  //               <li>React</li>
-  //               <li>Express</li>
-  //               <li>MySQL</li>
-  //               <li>Node.js</li>
-  //               <li>hosted on AWS EC2 & RDS</li>
-  //             </ul>
-  //           </div>
-  //         </div>
-  //       </div>
-
-  //       <a href="https://github.com/proj-plus-ultra/fenty-recommended" target="_blank">codebase</a>
-  //     </div>
-  //   )
-  // }
 }
 
 export default Ecommerce;
