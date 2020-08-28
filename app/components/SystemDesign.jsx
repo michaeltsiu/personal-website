27d62db793f3046ffa03218b456630059b0690cc
Michael Siu <michaeltsiu13@gmail.com>
2020-08-28 15:36:15 -0700

    Added a main image for System Design component

--- app/components/SystemDesign.jsx
+++ app/components/SystemDesign.jsx
@@ -1,28 +1,63 @@
 import React from 'react';
 
+import { MdKeyboardArrowDown } from "react-icons/md";
+let ref = React.createRef();
+
 class SystemDesign extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
 
     }
+    this.handleClick = this.handleClick.bind(this);
   }
+
+  handleClick() {
+    ref.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+
   render() {
     return (
-      <div>
-        This is System Design
-        <div>
-        Built with:
-          <ul>
-            <li>React</li>
-            <li>Express</li>
-            <li>PostgreSQL</li>
-            <li>Node.js</li>
-            <li>stress tested via New Relic & Loader.io</li>
-            <li>hosted on AWS EC2 t2.micros x 3</li>
-            <li>Load balancing & caching via NGINX</li>
-          </ul>
-          <a href="https://github.com/proj-minus-ultra/navbar-proxy" target="_blank">codebase</a>
+      <div className="portfolio-main">
+        <div className="portfolio-hero">
+          <div className="portfolio-video">
+            <img src="/app/assets/Portfolio/System-Design/System-Design_Overview.png" alt="IMAGE NOT FOUND" width="50%" height="auto" max-width="30vw" />
+          </div>
+
+          <div className="portfolio-tech">
+            <div>
+              <span>Built with:</span>
+              <div className="portfolio-system">
+                <ul>
+                  <li>React</li>
+                  <li>PostgreSQL</li>
+                  <li>AWS</li>
+                  <li>Ubuntu</li>
+                  <li>Artillery</li>
+                  <li>Loader.io</li>
+                  <li>NGINX</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="portfolio-arrow">
+            <div onClick={this.handleClick}>
+              <MdKeyboardArrowDown />
+            </div>
+          </div>
+        </div>
+
+        <div className="portfolio-text" ref={ref}>
+          <div className="portfolio-header">Overview</div>
+          <div className="portfolio-innertext">
+            <p></p>
+          </div>
+          <div>You can find the codebase of the System Design project <a href="https://github.com/proj-minus-ultra/navbar-proxy" target="_blank">here</a>
+          </div>
         </div>
       </div>
     )
